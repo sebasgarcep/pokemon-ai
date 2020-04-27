e722429374d70e5abb5b9ceb87403f98c30c919b
Sebastian Garrido <sebgar@gascaribe.com>
2020-04-26 20:34:54 -0500

    Explore how to set and pick teams in a battle

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
 const MetaTeamGenerator = require('./team-generators/MetaTeamGenerator');
 const RandomTeamGenerator = require('./team-generators/RandomTeamGenerator');
+const { Battle } = require('./simulator');
+const { formatid } = require('./constants');
 
 async function start() {
   const randomGen = new RandomTeamGenerator();
   await randomGen.init();
-  console.log(randomGen.generateTeam());
+  const p1 = randomGen.generateTeam();
+  console.log(p1);
 
   const metaGen = new MetaTeamGenerator();
   await metaGen.init();
-  console.log(metaGen.generateTeam());
+  const p2 = metaGen.generateTeam();
+  console.log(p2);
+
+  const options = {
+    debug: true,
+    formatid,
+    send: (type, data) => {
+      if (Array.isArray(data)) data = data.join('\n');
+      console.log(type, data);
+      if (type === 'end') { console.log('end'); }
+    }
+  };
+
+  const battle = new Battle(options);
+  battle.setPlayer('p1', { team: p1 });
+  battle.setPlayer('p2', { team: p2 });
+  battle.choose('p1', 'team 3456');
+  battle.choose('p2', 'team 3456');
+  console.log(battle.sides[0].requestState);
+  console.log(battle.sides[1].requestState);
 }
 
 start()
